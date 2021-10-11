68f5818b9be1b1703dbbdfe4434364c2e9e37cf7
Paulo Kim <paulojskim@usp.br>
2021-10-11 19:47:10 -0300

    chore: created coach details controller

---
+++ app/controllers/coach_details_controller.rb
@@ -0,0 +1,7 @@
+class CoachDetailsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :coach_details, only:[:index, :show]
+  
   root 'rails/welcome#index'
 end
