ec06c787e56f5cce94c64fe6a0af60b08fd26873
King-witcher <giuseppe.2000@hotmail.com>
2021-10-13 11:45:31 -0300

    trollei o rspec perae =D

--- spec/requests/whoami_spec.rb
+++ spec/requests/whoami_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Whoamis", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/whoami/index"
+      get "/whoami"
       expect(response).to have_http_status(:success)
     end
   end
