e84d62f4bcedf573dbae143d4f93a2d9e9be049d
King-witcher <giuseppe.2000@hotmail.com>
2021-10-12 23:26:32 -0300

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_193833) do
+ActiveRecord::Schema.define(version: 2021_10_11_224855) do
 
   create_table "coaches", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "whatsapp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "Is invalid if password and email are present" do
-    @invalid = User.new(username: FFaker::Name.first_name, password: 'Abcde123456_')
-    expect(@invalid).to be_valid;
+  it "Is valid if password and email are present" do
+    @valid = User.new(username: FFaker::Name.first_name, password: 'Abcde123456_')
+    expect(@valid).to be_valid;
   end
 
   it "Is invalid if username is nil" do
