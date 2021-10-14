0258b706720a93b1e1fdffea9b3a23f616c00e3f
Gabriel Melo <oliveira.8102@usp.br>
2021-10-13 21:56:44 -0300

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'rails/welcome#index'
 
   get "/games" => "games#show_games"
-  get "/coaches" => "coaches#choose_game"
+  #get "/coaches" => "coaches#choose_game"
 end
 
 
