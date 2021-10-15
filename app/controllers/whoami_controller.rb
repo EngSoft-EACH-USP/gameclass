585d9e8a0323496e12ec15d295d650914b22de7b
King-witcher <giuseppe.2000@hotmail.com>
2021-10-14 21:24:46 -0300

    Agora o site mostra o nome ao invés do username.

--- app/controllers/whoami_controller.rb
+++ app/controllers/whoami_controller.rb
@@ -2,8 +2,8 @@
 class WhoamiController < ApplicationController
     def index
         if session[:logged]
-            @user = User.find_by(id: session[:user_id])
-            render plain: "You are logged as #{@user.username}."
+            user = User.find_by(id: session[:user_id])
+            render plain: "You are logged as #{user.name}."
         else
             render plain: 'You are not logged in.'
         end
