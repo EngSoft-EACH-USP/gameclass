44d9b9145346eeda1ece243cbe9de7171d1f4265
King Witcher <giuseppe.2000@hotmail.com>
2021-10-18 12:52:14 -0300

    Cobertura de testes para PseronalController e WhoamiController. Se nao der 100% agora eu tranco o curso.

---
+++ spec/controllers/whoami_controller_spec.rb
@@ -0,0 +1,29 @@
+
+require 'rails_helper'
+
+RSpec.describe WhoamiController, type: :controller do
+  context 'when logged' do
+    before do
+      @user = build :user
+      controller.stub(:isLogged?).and_return true
+      controller.stub(:current_user).and_return @user
+    end
+
+    it 'responds with the name' do
+      get :index
+      expect(response.body).to include @user.name
+    end
+  end
+
+  context 'when not logged' do
+    before do
+      controller.stub(:isLogged?).and_return false
+      controller.stub(:current_user).and_return nil
+    end
+
+    it 'tells not logged' do
+      get :index
+      expect(response.body).to include 'not logged'
+    end
+  end
+end
