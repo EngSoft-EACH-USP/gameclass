57c33e90f21ec40182de35112d16442cee472d75
Paulo Kim <paulojskim@usp.br>
2021-10-31 16:15:35 -0300

    testing view

---
+++ spec/views/affiliate/show.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "affiliate/show.html.erb", type: :view do
+  before do
+    assign(:user, FactoryBot.build(:user))
+    assign(:affiliates, [
+      AffiliateRequest.create!(user_id: 1, description: 'Sou bom no lol, diamante', status: :pending),
+    ])
+  end
+
+  it 'should render coach details page' do
+    render
+
+    rendered.should =~ /Affiliates/
+  end
+
+  it 'should not render coach details page' do
+    assign(:affiliates, [])
+    render
+
+    rendered.should =~ /Nao existem solicitacoes/
+  end
+end
