56b0550bf9b0529bf57d2f8126880c8c0e306f3a
Paulo Kim <paulojskim@Usp.br>
2021-11-07 16:41:45 -0500

    Fixed github actions

--- features/step_definitions/before_statements.rb
+++ features/step_definitions/before_statements.rb
@@ -1,22 +1,22 @@
 
 Before '@mock_user' do
-    user = FactoryBot::create :user
+    user = FactoryBot::create :user, :id => 1
     @username = user.username
     @password = user.password
     @name = user.name
 end
 
 Before '@mock_coach' do
-    user = FactoryBot::create :user, :id => 1
-    coach = FactoryBot::create :coach, :user_id => 1, :whatsapp => 11980770907
+    user = FactoryBot::create :user, :id => 2
+    coach = FactoryBot::create :coach, :user_id => 2, :whatsapp => 11980770907
     coach.save
 end
 
 Before '@mock_affiliate_request' do
-    user = FactoryBot::create :user, :id => 2, :name => 'aceitado'
-    user = FactoryBot::create :user, :id => 3, :name => 'rejeitado'
-    affiliate_accept = FactoryBot::create :affiliate_request, :user_id => 2
-    affiliate_reject = FactoryBot::create :affiliate_request, :user_id => 3
+    user = FactoryBot::create :user, :id => 6, :name => 'aceitado'
+    user = FactoryBot::create :user, :id => 7, :name => 'rejeitado'
+    affiliate_accept = FactoryBot::create :affiliate_request, :user_id => 6
+    affiliate_reject = FactoryBot::create :affiliate_request, :user_id => 7
     affiliate_accept.save
     affiliate_reject.save
 end
