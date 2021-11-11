efad57ac89c342e4daf876fef15616d45f4efb87
King-witcher <giuseppe.2000@hotmail.com>
2021-11-11 16:40:16 -0300

    Actions: Testes Cucumber e RSpec separados.
    Ele deverá falhar "com sucesso".

--- spec/purposely_fail_spec.rb
+++
@@ -1,9 +0,0 @@
-# Esse spec foi feito exclusivamente para testar o GitHub actions
-
-require 'rails_helper'
-
-RSpec.describe "Testing if github actions fails correctly." do
-    it "just fails" do
-        expect(true).to eq false
-    end
-end
