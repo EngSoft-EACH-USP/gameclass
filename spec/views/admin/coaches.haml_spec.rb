602da89997e2d2148f44c8bbc8559fdaa1b4abec
King-witcher <giuseppe.2000@hotmail.com>
2021-11-17 13:18:43 -0300

    Testes RSpec implementados (depois da funcionalidade mesmo)

--- spec/views/admin/coaches.haml_spec.rb
+++ spec/views/admin/coaches.haml_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "admin/coaches.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'shows infos about each coach' do
+    user = build :user
+    coach = build :coach, user: user
+    coach_list = [ coach ]
+    assign(:coaches, coach_list)
+    render
+    expect(rendered).to match /#{user.name}/
+    expect(rendered).to match /#{coach.game}/
+    expect(rendered).to match /#{coach.whatsapp}/
+  end
 end
