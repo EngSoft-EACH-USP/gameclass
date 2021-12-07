44aee3c56681f6c7fd54c23ee292de191d2afa52
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2021-12-07 14:57:44 -0300

    Agora o teste que media a falha está passando, mas isso mostrou que outros testes não estavam funcionando corretamente.

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -14,38 +14,42 @@ class InfosController < ApplicationController
   end
 
   def update
-      # Esses dois campos são de instância para poderem ser acessados de volta na view.
-      @id = params[:id]
-      @user = User.find(params[:id])
-      @username = params[:username]
-      @name = params[:name]
-      password = params[:password]
-      @password = password
+    unless is_logged?
+      head 401
+    end
+    unless params[:id] == current_user.id
+      head 403
+    end
 
-      if password != params[:check_password]
-        @different_passwords = true
-        render :forms
+    @user = User.find(params[:id])
+    @username = params[:username]
+    @name = params[:name]
+    password = params[:password]
+    @password = password
 
-      elsif User.find_by(username: @username)
-        @username_unavaliable = true
-        render :forms
+    if password != params[:check_password]
+      @different_passwords = true
+      render :forms
 
-      else
-        if @password != ''
-          @user.password = @password
-        end
+    elsif User.find_by(username: @username)
+      @username_unavaliable = true
+      render :forms
 
-        if @username != ''
-          @user.username = @username
-        end
-
-        if @name != ''
-          @user.name = @name
-        end
-
-        @user.save
-        redirect_to '/me'
+    else
+      if @password != ''
+        @user.password = @password
       end
-  end
 
+      if @username != ''
+        @user.username = @username
+      end
+
+      if @name != ''
+        @user.name = @name
+      end
+
+      @user.save
+      redirect_to '/me'
+    end
+  end
 end
